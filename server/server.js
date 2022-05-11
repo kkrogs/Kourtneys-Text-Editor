38d719c89dc699df14995b9e338bb9e1bd0f6041
kkrogs <kourtney.krogman@gmail.com>
2022-05-10 19:24:38 -0600

    server.js adjusting

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// requiring HTML routes
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
